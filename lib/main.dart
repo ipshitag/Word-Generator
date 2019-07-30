d7aa8ed37d047c074fd41b2a63a4269fcc8d0a6b
ipshitag <iuippi3@gmail.com>
2019-07-31 02:14:19 +0530

    main commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,15 +10,53 @@ class MyApp extends StatelessWidget
   {
     return MaterialApp
       (
-          title: "Welcome to my first flutter app",
-          home: Scaffold(
-            appBar: AppBar(
-              title: Text('Hello'),
-            ),
-            body: Center(
-              child: Text('Hello world'),
-            )
-          ),
-    );
+          title: "Word Generator",
+          home: RandomWords() );
   }
 }
+
+class RandomWordState extends State<RandomWords>
+{
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(appBar: AppBar(title: Text('Word Generator'),),body: _buildSuggestions(),);
+  }
+
+  Widget _buildSuggestions () {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i )
+    {
+      if(i.isOdd) return Divider();
+      final index = i ~/ 2;
+      if( index > _suggestions.length)
+        {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+      return _buildRow(_suggestions[index]);
+    }
+    );
+  }
+
+  Widget _buildRow (WordPair pair)
+  {
+    return ListTile(
+      title : Text(pair.asPascalCase,
+      style: _biggerFont),
+    );
+
+  }
+}
+
+
+class RandomWords extends StatefulWidget
+{
+  @override
+  RandomWordState createState() => new RandomWordState();
+
+}
+
